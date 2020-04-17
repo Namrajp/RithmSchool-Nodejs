9a58b5a5c14b11996ea92d5d71f7ec404d3abcd0
Nammaraj <active99raj@gmail.com>
2020-04-17 19:36:03 +1000

    more helpers.js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 const helpers = require("./helpers");
+const moreHelpers = require("./moreHelpers");
 helpers.sayHi();
+
+console.log(moreHelpers.firstName);
+moreHelpers.sayHello();
+moreHelpers.sayGoodbye();
